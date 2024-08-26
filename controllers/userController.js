6d8eef7d9d71387bf085e70aad2019dde0c48fb2
Boss Eggs <wesley.j.locklair@gmail.com>
2024-08-26 19:04:12 -0400

    changed one thing

--- controllers/userController.js
+++ controllers/userController.js
@@ -59,7 +59,7 @@ async deleteUser(req, res) {
     if (!user) {
       res.status(404).json({ message: 'No user with that ID' });
     }
-
+    res.json(user);
   } catch (err) {
     res.status(500).json(err);
   }
